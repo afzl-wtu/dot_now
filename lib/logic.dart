bb6e295636000c18f73ea0ed6cbe47d40377f05f
Muhammad Afzal Tahir <afzl.wtu@hotmail.com>
2022-04-01 19:48:22 +0500

    landing_pages done

---
+++ lib/logic.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import './screens/landing_page.dart';
+
+class Logic extends StatelessWidget {
+  const Logic({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LandingPage();
+  }
+}
